9604d3c596c6804da5f8981681af949ab5acaf29
katerine.martinez <katerine.martinez@gradiweb.com>
2021-07-17 23:55:54 -0500

    wip feature: comment form tablet

--- src/components/commentForm/CommentForm.jsx
+++ src/components/commentForm/CommentForm.jsx
@@ -5,10 +5,13 @@ const CommentForm = () => {
   return (
     <div className="CommentForm">
       <img className="CommentForm--pic" src={UserPic} alt="User Pic"/>
-      <div className="CommentForm__form">
-        <label className="CommentForm__form--label">JOIN THE CONVERSATION</label>
-        <textarea className="CommentForm__form--input" type="text" placeholder="Write comment here" />
-      <button className="CommentForm__form--button" onClick={() => { }}>COMMENT</button>
+      <div>
+        <div className="CommentForm__form">
+          <label className="CommentForm__form--label">JOIN THE CONVERSATION</label>
+          <textarea className="CommentForm__form--input" type="text" placeholder="Write comment here" />
+        </div>
+        <div>
+          <button className="CommentForm__form--button" onClick={() => { }}>COMMENT</button></div>
       </div>
     </div>
   )
