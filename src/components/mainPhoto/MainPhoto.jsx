29796471ccd5a8243987ba70e861f33156beab49
katerine.martinez <katerine.martinez@gradiweb.com>
2021-07-17 20:12:46 -0500

    WIP feature: main photo mobile

--- src/components/mainPhoto/MainPhoto.jsx
+++ src/components/mainPhoto/MainPhoto.jsx
@@ -4,8 +4,8 @@ import './MainPhoto.scss'
 const MainPhoto = (props) => {
     return (
         <div className="MainPhoto" key={props.video.id}>
-            <img className="MainPhoto__pic" src={props.video.image} alt="" style={{ height: '30rem' }} />
-            <div className="MainPhoto__timer">0:00/{props.video.duration}</div>
+            <img className="MainPhoto--video" src={props.video.image} alt="current video" />
+            <div className="MainPhoto--timer">0:00/{props.video.duration}</div>
         </div>
     );
 };
